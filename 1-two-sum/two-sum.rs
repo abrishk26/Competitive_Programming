44f2230a991b9ff4506c9fa10a77b10b0b8ca823
Abreham Kassa <abrehamkassa19@gmail.com>
2024-09-08 22:52:51 +0300

    Time: 1 ms (86.74%) | Memory: 2.3 MB (41.62%) - LeetSync

--- 1-two-sum/two-sum.rs
+++ 1-two-sum/two-sum.rs
@@ -1,20 +1,18 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        use std::collections;
-
-        let mut hash_table: collections::HashMap<i32, i32> = collections::HashMap::new();
-        let mut result: Vec<i32> = Vec::new();
-
+        
+        let mut hash_table: HashMap<i32, i32> = HashMap::new();
+    
         for i in 0..nums.len() {
             if hash_table.contains_key(&(nums[i])) {
-                result.push(i as i32);
-                result.push(hash_table.get(&(nums[i])).copied().unwrap());
-                break;
+               return vec![i as i32, hash_table.get(&(nums[i])).copied().unwrap()];
             } else {
                 hash_table.insert(target - nums[i], i as i32);
             }
         }
 
-        return result
+        unreachable!()
     }
 }
